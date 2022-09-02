c33a3c9395eb4fe3a290a95f4975ffbb7d8f878f
Gondwana7 <96773295+Gondwana7@users.noreply.github.com>
2022-09-03 00:10:13 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,181 @@
+const menu = [
+  {
+    id: 1,
+    title: "Tteokbokki",
+    category: "Korea",
+    price: 10.99,
+    img:
+      "https://twoplaidaprons.com/wp-content/uploads/2020/09/tteokbokki-top-down-view-of-tteokbokki-in-a-bowl-500x500.jpg",
+    desc: `Spicy rice cakes, serving with fish cake.`,
+  },
+  {
+    id: 2,
+    title: "Chicken Ramen",
+    category: "Japan",
+    price: 7.99,
+    img:
+      "https://www.forkknifeswoon.com/wp-content/uploads/2014/10/simple-homemade-chicken-ramen-fork-knife-swoon-01.jpg",
+    desc: `Chicken noodle soup, serving with vegetables such as soy bean, green onion. In an optional you can ask for egg. `,
+  },
+  {
+    id: 3,
+    title: "Bibimbap",
+    category: "Korea",
+    price: 8.99,
+    img:
+      "https://dwellbymichelle.com/wp-content/uploads/2020/05/DWELL-bibimbap.jpg",
+    desc: `Boiling vegetables, serving with special hot sauce`,
+  },
+  {
+    id: 4,
+    title: "Dan Dan Mian",
+    category: "China",
+    price: 5.99,
+    img:
+      "https://www.savingdessert.com/wp-content/uploads/2019/02/Dan-Dan-Noodles-10.jpg",
+    desc: `Dan dan noodle, serving with green onion `,
+  },
+  {
+    id: 5,
+    title: "Yangzhou Fried Rice",
+    category: "China",
+    price: 12.99,
+    img:
+      "https://salu-salo.com/wp-content/uploads/2013/02/Yangzhou-Fried-Rice1.jpg",
+    desc: `Yangzhou style fried rice, serving with bean and pickles `,
+  },
+  {
+    id: 6,
+    title: "Onigiri",
+    category: "Japan",
+    price: 9.99,
+    img:
+      "https://www.manusmenu.com/wp-content/uploads/2017/08/Onigiri-3-1-of-1.jpg",
+    desc: `Rice Sandwich, serving with soy sauce`,
+  },
+  {
+    id: 7,
+    title: "Jajangmyeon",
+    category: "Korea",
+    price: 15.99,
+    img:
+      "https://www.curiouscuisiniere.com/wp-content/uploads/2020/04/Jajangmyeon-Korean-Noodles-in-Black-Bean-Sauce5.1200H-720x540.jpg",
+    desc: `Black bean sauce noodle, serving with green onion `,
+  },
+  {
+    id: 8,
+    title: "Ma Yi Shang Shu",
+    category: "China",
+    price: 12.99,
+    img:
+      "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/F688C2F6-86EC-46C4-B9C7-A6BA01DF7437/Derivates/32E3E72A-F786-406D-AF7F-B30980A9AC6C.jpg",
+    desc: `Hot pepper sauce noodle, serving with soy bean and onion`,
+  },
+  {
+    id: 9,
+    title: "Doroyaki",
+    category: "Japan",
+    price: 3.99,
+    img:
+      "https://www.justonecookbook.com/wp-content/uploads/2011/10/Dorayaki-New-500x400.jpg",
+    desc: `Red bean paste dessert, serving with honey.`,
+  },
+  {
+    id: 10,
+    title: "Cheong Fun",
+    category: "Hong Kong",
+    price: 13.99,
+    img:
+      "https://blog.biletbayi.com/wp-content/uploads/2018/02/Cheong-Fun-scaled.jpg",
+    desc: `It is prepared by wrapping the shrimp or meat between the dough.`,
+  },
+];
+
+
+const menuItems = document.querySelector(".menu-items")
+const btnContainer = document.querySelector(".btn-container")
+
+
+window.addEventListener("DOMContentLoaded", function () {
+  displayMenuItems(menu)
+  displayMenuBtns()
+})
+
+
+
+
+function displayMenuBtns(){
+  const categories = menu.reduce(function (values, item) {
+
+    if (!values.includes(item.category)) {
+      values.push(item.category)
+    }
+    return values
+  
+  }, ["all"])
+  
+  const categoryBtns = categories.map(function (category) {
+    return `<button class="filter-btn" data-id="${category}">${category}</button>`
+  }).join("")
+  
+  btnContainer.innerHTML = categoryBtns;
+  
+  const filterBtns = document.querySelectorAll(".filter-btn")
+  
+  
+  
+  filterBtns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+      const category = e.currentTarget.dataset.id
+      const menuCategory = menu.filter(function (menuItem) {
+        if (menuItem.category === category) {
+  
+          return menuItem
+  
+        }
+      })
+  
+      if (category === "all") {
+        displayMenuItems(menu)
+      } else {
+        displayMenuItems(menuCategory)
+        console.log(btn);
+      }
+  
+    })
+  })
+}
+
+function displayMenuItems(menuProducts) {
+  let displayMenu = menuProducts.map(function (products) {
+
+    return `<div class="item" style="border: 1px solid;">
+  
+  
+      <div class="image">
+        <img src=${products.img} alt="">
+      </div>
+  
+  
+      <div class="info ">
+  
+        <div class="d-flex card-header">
+          <h4 class="title">
+          ${products.title}
+            <h4 class="price" style="margin-left: auto;">$${products.price}</h4>
+          </h4>
+  
+        </div>
+  
+        <div class="text">
+          <p>${products.desc}</p>
+        </div>
+  
+      </div>
+    </div>`
+
+  })
+  displayMenu = displayMenu.join("")
+  menuItems.innerHTML = displayMenu
+}
+
